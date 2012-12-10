e04e01d38199434317c59efd2bdd0284e54cff13
MosheShpilman <oshesh@post.jce.ac.il>
2012-12-10 23:39:18 +0200

    Green

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,10 +1,50 @@
 
 public class RomanNumerals {
+	
+	String digitToString(int digit, String one, String five, String ten)
+	{
+		String result=null;
+		switch (digit)
+		{
+		case 1: result=one;
+				break;
+		case 2:	result=one+one;
+				break;
+		case 3: result=one+one+one;
+				break;
+		case 4: result=one+five;
+				break;
+		case 5: result=five;
+				break;
+				
+		}
+		return result;
+	}
 
 	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+		int temp = nmber;
+		int count = 0;
+		while(temp!=0)
+		{
+			temp=temp/10;
+			count++;
+		}
+		
+		temp=nmber;
+		int digit;
+		String result = "";
+		for(int i=1; i<=count;i++)
+		{
+			digit=temp%10;
+			temp=temp/10;
+			switch(i)
+			{
+				case 1:
+				result = digitToString(digit,"I","V","X");
+			}
+		}
+		return result;
+		
 	}
 
 }
